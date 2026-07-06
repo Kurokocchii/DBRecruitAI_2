8ad35a626fc819cfa20072d7bd4df52f28291c80
Kurokocchii <142899533+Kurokocchii@users.noreply.github.com>
2026-07-06 14:59:42 +0800

    Modify settings.py for static and media files
    Updated Django settings to include static and media file configurations.

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
+
+MEDIA_URL ="/media/"
 MEDIA_ROOT = BASE_DIR / "media"
